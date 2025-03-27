4b11a01321332a56577dcc9ee8f1d0c4b373f546
Akshat Ranakoti <akshatranakoti01@gmail.com>
2025-03-27 21:36:45 +0530

    user model added

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -5,6 +5,7 @@ from watchlist_app.models import StreamPlatform
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    review_user=serializers.StringRelatedField(read_only=True)
     class  Meta:
         model=Review
         exclude=('watchlist',)
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -8,13 +8,21 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import viewsets 
+from rest_framework.exceptions import ValidationError
 class ReviewCreate(generics.CreateAPIView):
     serializer_class=ReviewSerializer
+
+    def get_queryset(self):
+        return  Review.objects.all()
 
     def perform_create(self,serializer):
         pk=self.kwargs.get('pk')
         watchlist=WatchList.objects.get(pk=pk)
-        serializer.save(watchlist=watchlist)
+        review_user=self.request.user
+        review_queryset=Review.objects.filter(watchlist=watchlist,review_user=review_user)
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie")
+        serializer.save(watchlist=watchlist,review_user=review_user)
     
 class ReviewList(generics.ListAPIView):
     
