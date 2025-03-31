d9fb28aed197f3589d5318c43463e99f85916b6b
Akshat Ranakoti <akshatranakoti01@gmail.com>
2025-03-31 12:10:45 +0530

    permissions and calculations added

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -9,6 +9,9 @@ from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import viewsets 
 from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
+from watchlist_app.api.permissions import AdminOrReadOnly,ReviewUserOrReadOnly
+
 class ReviewCreate(generics.CreateAPIView):
     serializer_class=ReviewSerializer
 
@@ -22,16 +25,23 @@ class ReviewCreate(generics.CreateAPIView):
         review_queryset=Review.objects.filter(watchlist=watchlist,review_user=review_user)
         if review_queryset.exists():
             raise ValidationError("You have already reviewed this movie")
+        if watchlist.number_rating==0:
+            watchlist.avg_rating=serializer.validated_data['rating']
+        else:
+            watchlist.number_rating=watchlist.number_rating+1
+            watchlist.avg_rating=(watchlist.avg_rating+serializer.validated_data['rating'])/2
+        watchlist.save()
         serializer.save(watchlist=watchlist,review_user=review_user)
     
 class ReviewList(generics.ListAPIView):
-    
+    permission_classes=[IsAuthenticated]
     serializer_class=ReviewSerializer
     def get_queryset(self):
         pk=self.kwargs['pk']
         return Review.objects.filter(watchlist=pk)
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=[ReviewUserOrReadOnly]
     queryset=Review.objects.all()
     serializer_class=ReviewSerializer
 
@@ -144,8 +154,8 @@ class WatchListAV(APIView):
 class WatchDetailAV(APIView):
     def get(self,request,pk):
         try:
-          movie=WatchListSerializer.objects.get(pk=pk)
-        except WatchListSerializer.DoesNotExist:
+          movie=WatchList.objects.get(pk=pk)
+        except WatchList.DoesNotExist:
             return Response({"Error":' not found'},status=status.HTTP_404_NOT_FOUND)
         serializer=WatchListSerializer(movie)
         return Response(serializer.data)  
