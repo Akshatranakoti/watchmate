49c4f86bd08255d968b1b589e0a6ab4e54836906
Akshat Ranakoti <akshatranakoti01@gmail.com>
2025-04-01 22:10:10 +0530

    permission and authentication

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -130,3 +130,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     ],
 
 # }
+
+REST_FRAMEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+     'rest_framework.authentication.BasicAuthentication',
+    ]
+}
